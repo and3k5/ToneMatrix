3710f4a186e17b37ce3666988117759bd36db71a
Anders K <ande7353@gmail.com>
2015-07-13 09:29:35 +0000

    Fix sound

--- js/ToneMatrix.js
+++ js/ToneMatrix.js
@@ -111,7 +111,79 @@ var Button_up = function () {
 
 
 
+
 var audioContext = new AudioContext();
+
+var sampleRate = audioContext.sampleRate;
+var sampleDuration = 0.25;
+var sampleLength = sampleRate*sampleDuration;
+
+function SineBuffer(audioContext,freq) {
+    var buffer = audioContext.createBuffer(1, sampleLength, sampleRate);
+    
+    var data = buffer.getChannelData(0);
+    
+    
+    for (var i = 0; i < sampleLength; i++) {
+        // Math.random() is in [0; 1.0]
+        // audio needs to be in [-1.0; 1.0]
+        var ang = (i/sampleLength)*freq*(360*sampleDuration);
+        data[i] = Math.sin(ang*Math.PI/180)*(1-i/sampleLength);
+    }
+  
+    return buffer;
+}
+
+var Buffers = [];
+var BufferSources = [];
+
+var i=0;
+
+/*function DynamicBufferSource(buffer) {
+    this.buffer = buffer;
+    
+    Object.defineProperty(this,"bufferSource",{
+        get: function () {
+            return this._bufferSource;
+        },
+        set: function (v) {
+            this._bufferSource=v;
+            v.buffer=this.buffer;
+        }
+    })
+    
+    this.bufferSource = audioContext.createBufferSource();
+    
+    this.start = function (v) {
+        try {
+            this.bufferSource.stop(v)
+            this.bufferSource.disconnect();
+        }
+        catch (e) {
+            
+        }
+        this.bufferSource = audioContext.createBufferSource();
+        
+        this.bufferSource.start(v);
+        var dbs = this;
+        this.bufferSource.onended = function () {
+            console.log("ended");
+            dbs.stop(0);
+        }
+    }
+    
+    this.stop = function (v) {
+        this.bufferSource.stop(v)
+        this.bufferSource.disconnect();
+        this.bufferSource = audioContext.createBufferSource();
+    }
+}*/
+
+Pattern.forEach(function (n) {
+    Buffers[n]=SineBuffer(audioContext,noteToFreq(n));
+    
+    i++;
+})
 
 var Button = function (f) {
     var b=document.createElement("div");
@@ -126,7 +198,8 @@ var Button = function (f) {
     b.addEventListener("mousedown",Button_down);
     b.addEventListener("mouseup",Button_up);
     
-    b.gain = audioContext.createGain();
+    b.setAttribute("data-note",f);
+    /*b.gain = audioContext.createGain();
     b.gain.connect(audioContext.destination);
     
     b.oscillator = audioContext.createOscillator();
@@ -135,7 +208,7 @@ var Button = function (f) {
     
     b.gain.gain.value = 0;
     
-    b.oscillator.start(0);
+    b.oscillator.start(0);*/
     
     
     return b;
@@ -144,7 +217,7 @@ Row = function (n) {
     var r=document.createElement("div");
     r.classList.add("row");
     for (var i=0;i<buttonsCount;i++) {
-        r.appendChild(Button(noteToFreq(Pattern[buttonsCount-n-1])));
+        r.appendChild(Button((Pattern[buttonsCount-n-1])));
     }
     return r;
 },
@@ -177,17 +250,23 @@ var start = function () {
             for (var i=0;i<arr.length;i++) {
                 //var row = Array.prototype.indexOf.call(arr[i].parentElement.parentElement.children,arr[i].parentElement);
                 //console.log(row);
-                noteOn(arr[i].gain,gainvalue);
+                noteOn(arr[i],gainvalue);
             }
         }
     },(60/128/4)*1000)
 }
 
 function noteOn(a,v) {
-    a.gain.value = v;
-    setTimeout(function () {
-        a.gain.value = 0;
-    },50)
+    var src = audioContext.createBufferSource();
+    var gain = audioContext.createGain();
+    src.buffer = Buffers[parseInt(a.getAttribute("data-note"))];
+    src.connect(gain);
+    gain.connect(audioContext.destination);
+    
+    gain.gain.value = v*0.70;
+    
+    src.start(0);
+    
 }
 
 window.addEventListener("mouseup",Button_up);
